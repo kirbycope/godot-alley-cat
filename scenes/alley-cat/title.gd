1fabf65c4ae062d04ca3be0337133cc9dc543d93
Timothy Cope <kirbycope@gmail.com>
2024-10-01 17:28:30 -0700

    tap to start

--- scenes/alley-cat/title.gd
+++ scenes/alley-cat/title.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
+var swipe_start: Vector2
+var swipe_threshold := 50.0
+var tap_time_threshold := 0.3
+var tap_start_time: float = 0.0
 
 
 ## Called when the node leaves the scene tree.
@@ -22,8 +26,32 @@ func _ready() -> void:
 ## Called when there is an input event. The input event propagates up through the node tree until a node consumes it.
 func _input(event: InputEvent) -> void:
 
+	# Check if the input is a Touch event
+	if event is InputEventScreenTouch:
+		# [touch] screen just _pressed_
+		if event.is_pressed():
+			# Record initial touch position
+			swipe_start = event.position
+			# Record initial touch time
+			tap_start_time = Time.get_ticks_msec() / 1000.0
+		# [touch] screen just _released_
+		else:
+			# Record final touch position
+			var swipe_end = event.position
+			# Calculate the differece from start and end positions
+			var swipe_delta = swipe_end - swipe_start
+			# Calculate the difference from start and end times
+			var tap_duration = Time.get_ticks_msec() / 1000.0 - tap_start_time
+			# [tap] touch just _released_
+			if swipe_delta.length() < swipe_threshold and tap_duration < tap_time_threshold:
+				# Start the game
+				Globals.main.start()
+
 	# Check if the game is no started
 	if !Globals.main.game_started:
 
-		# Start the game
-		Globals.main.start()
+		# Check if the input is "start" or [Enter]
+		if event is InputEventKey and event.pressed and event.keycode == KEY_ENTER:
+
+			# Start the game
+			Globals.main.start()
